674d872460acb97e9a0e6e064253838464ed7d8e
robbie.marquez@gmail.com <robbie.marquez@gmail.com>
2026-02-23 05:38:16 +0000

    table and view materialization

---
+++ models/demo/customer.sql
@@ -0,0 +1,18 @@
+{{
+    config(materialized = 'table')
+}}
+
+with customer_src as
+(
+    select
+    CUSTOMER_ID,
+    FIRST_NAME,
+    LAST_NAME,
+    EMAIL,
+    PHONE,
+    COUNTRY,
+    CREATED_AT,
+    CURRENT_TIMESTAMP AS INSERT_DTS
+    from {{source('customer', 'CUSTOMER_SRC')}}
+)
+select * from customer_src
