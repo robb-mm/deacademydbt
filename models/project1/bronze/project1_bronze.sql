76699130afe62443121fbbe20a965522cff9a292
robbie.marquez@gmail.com <robbie.marquez@gmail.com>
2026-02-28 07:34:07 +0000

    configuration hierarchy (1. model specific, 2. dbt_project.xml, 3. profile credentials)

---
+++ models/project1/bronze/project1_bronze.sql
@@ -0,0 +1,10 @@
+{{
+    config
+    (
+        materialized = 'table'
+    )
+}}
+select 1 as id
+union
+select 2 as id
+
---
+++ models/project2/mart/project2_agg.sql
@@ -0,0 +1,9 @@
+{{
+    config
+    (
+        materialized = 'table',
+        schema = 'AGG'
+    )
+}}
+select * from {{ ref('project2_mart') }} where name in ('abc')
+
